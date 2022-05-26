d6e61da11edcb48c3b8f97c7e82e1a8982390f7e
Gloria <Ubahgloria16@gmail.com>
2022-05-26 20:58:13 +0100

    Update palindrome.py
    Program to determine if a word entered is a palindrome or not.

--- palindrome.py
+++ palindrome.py
@@ -4,17 +4,13 @@
 # find_anagrams("racecar") --> True
 
 
-def find_anagrams(word):
-    string1 = "hello"
-    string2 = "racecar"
-    string1 = string1.casefold()
-    string2 = string2.casefold()
+string = input("Enter a word: ")
+string = string.casefold()
     
-    reversed_string1 = reversed(string1)
-    reversed_string2 = reversed(string2)
-    if list(string1) == list(reversed_string1) or list(string2) == list(reversed_string2):
-        print("TRUE!")
-    else:
-        print("FALSE!")
-
-    return word
+reversed_string = reversed(string)
+if list(string) == list(reversed_string):
+    print(string)
+    print("TRUE!")
+else:
+    print(string)
+    print("FALSE!")
